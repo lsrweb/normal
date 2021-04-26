c8c9c723b9bf36f01035320de07c9980ad1b97a8
codesheep <2975971434@qq.com>
2021-04-26 18:11:31 +0800

    修改图标引入规则

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
 import './registerServiceWorker'
 import router from './router'
+import '@/icons'
+
 import store from './store'
+
+
+import './plugins/element.js'
+
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
+
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,40 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const port = process.env.port || process.env.npm_config_port || 9528
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+  },
+  chainWebpack(config) {
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  }
+}
