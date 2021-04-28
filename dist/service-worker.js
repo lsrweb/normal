0e11f63edf9796c984242e8a12d683e1700ffd67
codesheep <2975971434@qq.com>
2021-04-28 09:25:25 +0800

    change

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/normal/dist/precache-manifest.834021e23771cb340a652529fdf7251b.js"
+  "/normal/dist/precache-manifest.da915c0cebaa5746c2851b8df433eca9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "normal"});
