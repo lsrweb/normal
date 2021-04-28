13cbe08860bee5c1b8ef7dd04846017e30cd319b
codesheep <2975971434@qq.com>
2021-04-28 09:49:47 +0800

    change permission router

---
+++ dist/precache-manifest.1e75c885e03510c191b789dc051dd0b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a47d56ea0db31274f384be622d061b1",
+    "url": "/normal/dist/index.html"
+  },
+  {
+    "revision": "cefc430b596d3e447332b6c56d53ea2e",
+    "url": "/normal/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/normal/dist/robots.txt"
+  },
+  {
+    "revision": "95a6e5f8d7a2d45c1cdc",
+    "url": "/normal/dist/static/css/app.3056b20f.css"
+  },
+  {
+    "revision": "74b15ee319d4a65f4b61",
+    "url": "/normal/dist/static/css/chunk-elementUI.16da611a.css"
+  },
+  {
+    "revision": "a8d240926e298e5374b0",
+    "url": "/normal/dist/static/css/chunk-libs.902ebb66.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/normal/dist/static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/normal/dist/static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "95a6e5f8d7a2d45c1cdc",
+    "url": "/normal/dist/static/js/app.bffb9c04.js"
+  },
+  {
+    "revision": "74b15ee319d4a65f4b61",
+    "url": "/normal/dist/static/js/chunk-elementUI.2877066c.js"
+  },
+  {
+    "revision": "a8d240926e298e5374b0",
+    "url": "/normal/dist/static/js/chunk-libs.fe7d5b70.js"
+  },
+  {
+    "revision": "9967dfe89928760b0e3c",
+    "url": "/normal/dist/static/js/runtime.8d063de6.js"
+  }
+]);
